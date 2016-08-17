a98dd1668709bb38bfb6fcbc9d8ca0e3b3999564
Mike Fedosin <mfedosin@mirantis.com>
2016-08-17 20:09:47 +0300

    Small fixes

--- glare/cmd/api.py
+++ glare/cmd/api.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
-
-# Copyright 2010 United States Government as represented by the
-# Administrator of the National Aeronautics and Space Administration.
-# Copyright 2011 OpenStack Foundation
-# All Rights Reserved.
 #
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
+# Copyright (c) 2016 Mirantis, Inc.
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 
 """
 Glare (Glance Artifact Repository) API service
 """
 
+import os
 import sys
 
 import eventlet
@@ -29,6 +27,14 @@ from oslo_utils import encodeutils
 
 eventlet.patcher.monkey_patch(all=False, socket=True, time=True,
                               select=True, thread=True, os=True)
+
+# If ../glare/__init__.py exists, add ../ to Python search path, so that
+# it will override what happens to be installed in /usr/(local/)lib/python...
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
+                                   os.pardir,
+                                   os.pardir))
+if os.path.exists(os.path.join(possible_topdir, 'glare', '__init__.py')):
+    sys.path.insert(0, possible_topdir)
 
 import glance_store
 from oslo_config import cfg
