b9c863e351ae65095c1e3179181025c2b9d33583
Mike Fedosin <mikhail.fedosin.ext@nokia.com>
2017-05-25 16:09:18 +0300

    Add new common utils tests
    Change-Id: I38ac7accf5fd90e402072dc6074c3ca2f2d46509

--- glare/common/utils.py
+++ glare/common/utils.py
@@ -50,30 +50,6 @@ LOG = logging.getLogger(__name__)
 GLARE_TEST_SOCKET_FD_STR = 'GLARE_TEST_SOCKET_FD'
 
 
-def chunkreadable(iter, chunk_size=65536):
-    """Wrap a readable iterator with a reader yielding chunks of
-    a preferred size, otherwise leave iterator unchanged.
-
-    :param iter: an iter which may also be readable
-    :param chunk_size: maximum size of chunk
-    """
-    return chunkiter(iter, chunk_size) if hasattr(iter, 'read') else iter
-
-
-def chunkiter(fp, chunk_size=65536):
-    """Return an iterator to a file-like obj which yields fixed size chunks.
-
-    :param fp: a file-like object
-    :param chunk_size: maximum size of chunk
-    """
-    while True:
-        chunk = fp.read(chunk_size)
-        if chunk:
-            yield chunk
-        else:
-            break
-
-
 def cooperative_iter(iter):
     """Return an iterator which schedules after each
     iteration. This can prevent eventlet thread starvation.
@@ -174,7 +150,7 @@ class CooperativeReader(object):
                     # check should never fire. Regardless it still worths to
                     # make the check, as the code may be reused somewhere else.
                     if len(result) >= MAX_COOP_READER_BUFFER_SIZE:
-                        raise exception.LimitExceeded()
+                        raise exception.RequestEntityTooLarge()
                     self.position += len(chunk)
                 else:
                     try:
